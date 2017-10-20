8fb883bad9bbc5442746ddbff2c5557722e2d357
Dominic Létourneau <doumdi@gmail.com>
2017-10-20 09:57:59 -0400

    Starting conversion to cmake

---
+++ libAvKys/Demo/main.cpp
@@ -0,0 +1,51 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QDir>
+#include "main.h"
+
+int main(int argc, char* argv[])
+{
+    QCoreApplication app(argc,argv);
+
+    qDebug() << "Starting...";
+
+
+    //Tell the library where to look for plugins
+    QStringList searchPaths;
+    searchPaths << QDir::currentPath() + "/AvKysPlugins";
+    AkElement::setSearchPaths(searchPaths);
+
+
+    AkElement::listSubModules("MultiSrc");
+
+    //This should load the library. Let's see...
+    AkElementPtr MultiSrcPtr = AkElement::create("MultiSrc");
+
+    if (MultiSrcPtr)
+    {
+        qDebug() << "State: " << MultiSrcPtr->state();
+
+        QObject* subModule = MultiSrcPtr->loadSubModule("ffmpeg");
+        qDebug() << subModule;
+
+        if (subModule)
+        {
+            //Set media information
+            QMetaObject::invokeMethod(subModule,"setMedia",Q_ARG(QString,"rtsp://admin:admin@24.53.36.3:9001/live.sdp"));
+
+            //Start loop
+            QMetaObject::invokeMethod(subModule,"setState",Q_ARG(AkElement::ElementState,AkElement::ElementStatePlaying));
+
+
+        }
+
+
+
+    }
+
+
+
+    //Start event loop
+    return app.exec();
+
+}
---
+++ libAvKys/Demo/main.h
@@ -0,0 +1,34 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <ak.h>
+#include <akelement.h>
+
+
+#if 0
+class MultiSrcInterface : public QObject
+{
+    Q_OBJECT
+public:
+    MultiSrcInterface (QObject *parent = NULL)
+        : QObject(parent)
+    {
+
+    }
+
+public slots:
+
+    void setMedia(const QString &mediaName)
+    {
+
+    }
+
+
+signals:
+
+    void setMedia(const QString &name);
+
+}
+#endif
+
+#endif
